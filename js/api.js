ce1fdaa53e033d99a07a64dec2a911db547505bf
wokalaka233 <860992714@qq.com>
2026-07-31 01:59:09 +0800

    fix: switch to Tencent Cloud SCF proxy

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 /* Habit Tracker - API Layer
-   Supabase SDK via Vercel proxy (Tokyo region) */
+   Supabase SDK via Tencent Cloud SCF proxy */
 
-const SUPA_URL = '/api';
+const SUPA_URL = 'https://1461105845-iim8nclwca.ap-shanghai.tencentcs.com';
 const SUPA_KEY = 'sb_publishable_xi-u5divr9AoQHLL_G9eaw_4dWotEY9';
 
 var db = window.supabase.createClient(SUPA_URL, SUPA_KEY, {
